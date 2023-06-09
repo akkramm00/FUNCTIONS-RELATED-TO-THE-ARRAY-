dd79bc888f4b25a9bf991f6e33e8bc7fd44912d2
akkramm00 <127404065+akkramm00@users.noreply.github.com>
2023-06-09 14:26:46 +0000

    Initial Commit

---
+++ index.php
@@ -0,0 +1,38 @@
+<html>
+  <head>
+    <title>PHP Test</title>
+  </head>
+  <body>
+    <?php 
+// La fonction " array_keys " qui affiche les clès d'un tableau :
+// Tableau associatif => 
+
+$voitures = array(
+  "Citroen" => "DS3",
+  "renault" => "Clio",
+  "Peugeot" => "306"
+);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?> 
+
+  <!--
+  This script places a badge on your repl's full-browser view back to your repl's cover
+  page. Try various colors for the theme: dark, light, red, orange, yellow, lime, green,
+  teal, blue, blurple, magenta, pink!
+  -->
+  <script src="https://replit.com/public/js/replit-badge-v2.js" theme="dark" position="bottom-right"></script>
+  </body>
+</html>
